abd94b7dcc19d8fb663def0d26d4813b6c97d4b4
zhangsheng <zhangsheng@qq.com>
2020-07-08 17:51:44 +0800

    spring源码学习

--- src/main/java/cn/stu/Test_01.java
+++ src/main/java/cn/stu/Test_01.java
@@ -7,7 +7,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Scanner;
 
 /**
  * 测试类：
@@ -16,59 +15,7 @@ public class Test_01 {
 
 
     public static void main(String[] args) {
-        test4();
-    }
-
-    public static void test5(){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int oldArr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            oldArr[i] = sc.nextInt();
-        }
-        sc.close();
-        // 定义变量a,用来计算0的个数
-        int a = 0;
-        for (int i = 0; i < oldArr.length; i++) {
-            if (oldArr[0] == 0) {
-                a++;
-            }
-        }
-        // 确定新数组长度
-        int newArr[] = new int[n - a];
-        // 定义变量,用来表示newArr数组的下标值
-        int b = 0;
-        for (int i = 0; i < oldArr.length; i++) {
-            // 用选择结构将旧数组不为0的单位按顺序负值给新数组
-            if (oldArr[i] != 0) {
-                newArr[b] = oldArr[i];
-                // 控制,新数组的下标
-                b++;
-            }
-        }
-        System.out.print("新数组 newArr 的值为: ");
-        for (int i = 0; i < newArr.length; i++) { //输出新数组
-            System.out.print(newArr[i] + " ");
-        }
-    }
-
-
-    public static void test4() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入数组长度:");
-        int next = scanner.nextInt();
-        int[] arry = new int[next];
-        for (int i = 0; i < next; i++) {
-            System.out.println("请输入第" + (i + 1) + "个数.");
-            int value = scanner.nextInt();
-            if (value != 0) {
-                arry[i] = value;
-            }
-        }
-        System.out.println("数组为：");
-        for (int i = 0; i < arry.length; i++) {
-            System.out.print(arry[i] + " ");
-        }
+        test3();
     }
 
     public static void test3() {
